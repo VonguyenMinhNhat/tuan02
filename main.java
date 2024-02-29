77795f3a14e5d56d815d8db0ced9f098f309e297
VonguyenMinhNhat <160804783+VonguyenMinhNhat@users.noreply.github.com>
2024-02-29 11:55:07 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,17 +1,48 @@
-    package src;
-
-
-
-
-    public class main {
-        public static void main(String[] args) {
-            Calculation cal1 = new Calculation(5, 7);
-            System.out.println(cal1.sum());
-            System.out.println( cal1.minus());
-            System.out.println(cal1.mul());
-            System.out.println(cal1.div());
-            System.out.println("stop!");
-
-        }
+package src;
+ class Shape {
+    public double calculateArea() {
+        return 0.0;
     }
+}
+
+
+class Circle extends Shape {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Diện tích hình tròn: " + circle.calculateArea());
+
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        System.out.println("Diện tích hình chữ nhật: " + rectangle.calculateArea());
+    }
+}
+
 
